8e322c8c0453817bb0cbbefed89d601710890e49
vedant-rmgd <vedant.ramgade@gmail.com>
2025-11-01 22:39:07 +0530

    improve sign up controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -7,50 +7,50 @@ import { generateToken } from "../utils/gen-JWT-token.js";
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
 
+    // validate all fields
     try {
-        // validate all fields
         if ([fullName, email, password].some((data) => data?.trim() === "")) {
-            throw new apiError(400, "all fields are required");
+            throw new apiError(400, "All fields are required");
         }
-
+    
+        if (password.length < 6) {
+            throw new apiError(400, "Password must be atleast 6 characters");
+        }
+    
         // check if user already exist or not
         const existedUser = await User.findOne({ email });
-
+    
         if (existedUser) {
-            throw new apiError(400, "user with this email already exist");
+            throw new apiError(400, "User with this email already exist");
         }
-
+    
         // now hash the user password
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
-
+    
         // now register the user in our database
         const newUser = await User.create({
             fullName: fullName,
             email: email,
             password: hashedPassword,
         });
-
+    
         if (newUser) {
             // generate JWT token
             generateToken(newUser._id, res);
             await newUser.save();
-
+    
             return res
                 .status(200)
                 .json(
-                    new apiResponse(
-                        200,
-                        newUser,
-                        "user is successfully signed in"
-                    )
+                    new apiResponse(200, newUser, "user is successfully signed in")
                 );
         } else {
             throw new apiError(400, "problem in registering the user");
         }
     } catch (error) {
-        console.log("ERROR -> ", error);
-        throw new apiError(500, "error in sigh up controller");
+        console.log("error -> ", error)
+        throw new apiError(400, error?.message)
     }
 };
 
