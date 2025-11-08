4457d44063d73e9799c888d9ad3d07236edf8c97
vedant-rmgd <vedant.ramgade@gmail.com>
2025-11-08 13:04:23 +0530

    fix: express v5 wildcard route issue for render

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,9 +25,9 @@ app.use("/api/message", messsageRoute);
 if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-    app.get("/*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-    })
+    app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
 }
 
 server.listen(PORT, () => {
