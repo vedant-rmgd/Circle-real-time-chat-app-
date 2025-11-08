d62440480a006b00c20641b095dd120506e4c77f
vedant-rmgd <vedant.ramgade@gmail.com>
2025-11-08 12:47:21 +0530

    fix: updated wildcard route for express v5 compatibility

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,7 +25,7 @@ app.use("/api/message", messsageRoute);
 if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-    app.get("*", (req, res) => {
+    app.get("/*", (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
     })
 }
